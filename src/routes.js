3a96f105e1807b6dee22a1a5be23eecffd92ff32
Jucelino Novais <jucelinodev@gmail.com>
2020-01-11 14:37:52 -0300

    add yup validator and valid register user

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express'
 
 import UserController from './auth/controllers/UserController'
 
+import ValidateUserStore from './auth/validator/UserStore'
+
 const router = Router()
 
 router.get('/', (req, res) => {
   res.json({ message: 'ok' })
 })
 
-router.post('/user', UserController.store)
+router.post('/user', ValidateUserStore, UserController.store)
 
 export default router
