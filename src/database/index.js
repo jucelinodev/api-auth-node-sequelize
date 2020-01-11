2ae2d8ed1b34d640ff39fae85b22b4ca29dabb5e
Jucelino Novais <jucelinodev@gmail.com>
2020-01-10 22:07:17 -0300

    create user model

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,21 @@
 import Sequelize from 'sequelize'
-import databaseConfig from '../config/databaseConfig'
+import User from '../auth/models/User'
+
+import databaseConfig from '../config/database'
+
+const models = [User]
 
 class Database {
-  constructor () {
+  constructor() {
     this.init()
   }
 
-  init () {
+  init() {
     this.connection = new Sequelize(databaseConfig)
+
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
   }
 }
 
